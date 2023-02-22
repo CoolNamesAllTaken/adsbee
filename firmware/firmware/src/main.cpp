d2553469f01aa3354bb6c0d50c787148bcff3896
John McNelly <jkailimcnelly@gmail.com>
2023-02-22 15:38:58 -0800

    Bringup PIO example and add ADSB waveforms

--- firmware/firmware/src/main.cpp
+++ firmware/firmware/src/main.cpp
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "blink.pio.h"
 #include "pico/binary_info.h"
-#include "scbs.hh"
-
-
-SCBS * scbs = NULL;
 
 int main() {
-    bi_decl(bi_program_description("SCBS-Pico Single Cell Battery Simulator"));
-    // bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+    bi_decl(bi_program_description("ADS-Bee ADSB Receiver"));
 
     stdio_init_all();
 
-    SCBS::SCBSConfig_t scbs_config;
-    scbs = new SCBS(scbs_config);
-    scbs->Init();
+    static const uint led_pin = 25;
+    static const float pio_freq = 2000;
+
+    // Choose PIO instance (0 or 1)
+    PIO pio = pio0;
+
+    // Get first free state machine in PIO 0
+    uint sm = pio_claim_unused_sm(pio, true);
+
+    // Add PIO program to PIO instruction memory. SDK will find location and
+    // return with the memory offset of the program.
+    uint offset = pio_add_program(pio, &blink_program);
+
+    // Calculate the PIO clock divider
+    float div = (float)clock_get_hz(clk_sys) / pio_freq;
+
+    // Initialize the program using the helper function in our .pio file
+    blink_program_init(pio, sm, offset, led_pin, div);
+
+    // Start running our PIO program in the state machine
+    pio_sm_set_enabled(pio, sm, true);
+
+    // Do nothing
+    while (true) {
+        sleep_ms(1000);
+    }
+
+    
 
     while (true) {
-        scbs->Update();
+
     }
 }
