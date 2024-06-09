0609343c4481303c2805c130da9f22ec25f07c48
John McNelly <jkailimcnelly@gmail.com>
2024-06-09 00:33:21 -0700

    Add additional fields to MAVLINK decoder

--- firmware/scripts/mavlink_decoder/mavlink_decoder.py
+++ firmware/scripts/mavlink_decoder/mavlink_decoder.py
@@ -12,11 +12,13 @@ mavlink_connection = mavutil.mavlink_connection(SERIAL_PORT, baud=BAUD_RATE)
 while (True):
     next_update_timestamp_s = time.time() + 1 / REFRESH_RATE_HZ
     os.system('cls') # Clear the screen.
-    print("ICAO Addr |Latitude  |Longitude |Alt Type  |Alt (m)   |Hdg (deg) |Hvel (m/s)|Vvel (m/s)|Callsign  |Type      |Squawk")
-    print("----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------")
+    print("ICAO Addr |Latitude  |Longitude |Alt Type  |Alt (m)   |Hdg (deg) |Hvel (m/s)|Vvel (m/s)|Callsign  |Type      |TSLC (sec)|Flags     |Squawk")
+    print("----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------|----------")
     msg = mavlink_connection.recv_match(type='ADSB_VEHICLE', blocking=False)
     while (msg):
         print(f"{msg.ICAO_address:10x}|{msg.lat / 1e7:+10.4f}|{msg.lon / 1e7:+10.4f}|{msg.altitude_type:10x}|"\
-              f"{msg.altitude / 1e3:10.2f}|{msg.heading / 100:10.4f}")
+                f"{msg.altitude / 1e3:10.2f}|{msg.heading / 100:10.4f}|{msg.hor_velocity / 100:10}|"\
+                f"{msg.ver_velocity / 100:10}|{msg.callsign:10}|{msg.emitter_type:10}|{msg.tslc:10}|{msg.flags:10b}"\
+                f"{msg.squawk:10o}")
         msg = mavlink_connection.recv_match(type='ADSB_VEHICLE', blocking=False)
     time.sleep(next_update_timestamp_s - time.time())
