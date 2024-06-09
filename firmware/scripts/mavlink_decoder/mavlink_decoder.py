7248c0c7c458fbf7201aad41f454fa4883d348ca
John McNelly <jkailimcnelly@gmail.com>
2024-06-09 01:22:24 -0700

    Update README

--- firmware/scripts/mavlink_decoder/mavlink_decoder.py
+++ firmware/scripts/mavlink_decoder/mavlink_decoder.py
@@ -18,7 +18,7 @@ while (True):
     while (msg):
         print(f"{msg.ICAO_address:10x}|{msg.lat / 1e7:+10.4f}|{msg.lon / 1e7:+10.4f}|{msg.altitude_type:10x}|"\
                 f"{msg.altitude / 1e3:10.2f}|{msg.heading / 100:10.4f}|{msg.hor_velocity / 100:10}|"\
-                f"{msg.ver_velocity / 100:10}|{msg.callsign:10}|{msg.emitter_type:10}|{msg.tslc:10}|{msg.flags:10b}"\
+                f"{msg.ver_velocity / 100:10}|{msg.callsign:10}|{msg.emitter_type:10}|{msg.tslc:10}|{msg.flags:10b}|"\
                 f"{msg.squawk:10o}")
         msg = mavlink_connection.recv_match(type='ADSB_VEHICLE', blocking=False)
     time.sleep(next_update_timestamp_s - time.time())
