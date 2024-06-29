5f7df41951d41ddc354f032b428230b80b97ae2b
John McNelly <jkailimcnelly@gmail.com>
2024-06-29 15:57:53 -0700

    Fix tests for target debug

--- firmware/pico/src/hal/hal.cc
+++ firmware/pico/src/hal/hal.cc
@@ -1,11 +1,7 @@
-#include "hal/hal.hh"
+#include "hal.hh"
 
 #include "pico/stdlib.h"
 
-uint64_t get_time_since_boot_us() {
-    return to_us_since_boot(get_absolute_time());
-}
+uint64_t get_time_since_boot_us() { return to_us_since_boot(get_absolute_time()); }
 
-uint32_t get_time_since_boot_ms() {
-    return to_ms_since_boot(get_absolute_time());
-}
+uint32_t get_time_since_boot_ms() { return to_ms_since_boot(get_absolute_time()); }
