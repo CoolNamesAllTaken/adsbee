f9e1257b72d5471db4ec1e334c372d012d07f19c
Garrett Battaglia <gbattaglia@idinnovators.com>
2024-07-27 16:48:09 -0500

    refactor systick time source class

--- firmware/pico/src/time/SysTickTimeSource.cpp
+++ firmware/pico/src/time/SysTickTimeSource.cpp
@@ -1,7 +1,44 @@
 #include "SysTickTimeSource.h"
 
-std::shared_ptr<adsbee::time::SysTickTimeSource> adsbee::time::SysTimeTimeSourceFactory::instance = nullptr;
-bool adsbee::time::SysTimeTimeSourceFactory::created = false;
-bool adsbee::time::SysTimeTimeSourceFactory::isExternal;
-adsbee::hal::Mutex adsbee::time::SysTickTimeSource::_mutex;
-volatile uint64_t adsbee::time::SysTickTimeSource::_interruptCount;
+#include <hardware/exception.h>
+#include <hardware/structs/systick.h>
+
+#include <mutex>
+
+namespace adsbee::time {
+std::shared_ptr<SysTickTimeSource> SysTimeTimeSourceFactory::instance = nullptr;
+bool SysTimeTimeSourceFactory::created = false;
+bool SysTimeTimeSourceFactory::isExternal;
+hal::Mutex SysTickTimeSource::_mutex;
+volatile uint64_t SysTickTimeSource::_interruptCount;
+
+SysTickTimeSource::SysTickTimeSource(bool external)
+    : _external(external),
+      CLOCK_RESET_VAL(_external ? 12000000 /* 1 seconds at 12MHz */ : 12500000 /* 0.1 seconds at 125Mhz */) {
+    systick_hw->csr = 0 |                      /*enable, false*/
+                      (1 << 1) |               /*interrupt enable, true*/
+                      (external ? 0 : 1 << 2); /*clock source*/
+
+    systick_hw->rvr = CLOCK_RESET_VAL;
+    systick_hw->cvr = 0; /* Clear current value */
+    systick_hw->csr = systick_hw->csr | 1;
+
+    exception_set_exclusive_handler(SYSTICK_EXCEPTION, handleIrq);
+}
+
+SysTickTimeSource::~SysTickTimeSource() { systick_hw->csr = 0; }
+
+uint64_t SysTickTimeSource::getTickCount() {
+    // todo: prevent deadlock with IRQ
+    std::lock_guard lock(_mutex);
+    return CLOCK_RESET_VAL * _interruptCount + systick_hw->cvr;
+}
+
+void SysTickTimeSource::handleIrq() {
+    // todo: prevent deadlock with getTickCount()
+    std::lock_guard lock(_mutex);
+    // increment on volatile vars is deprecated
+    _interruptCount = _interruptCount + 1;
+}
+
+}  // namespace adsbee::time
--- firmware/pico/src/time/SysTickTimeSource.h
+++ firmware/pico/src/time/SysTickTimeSource.h
@@ -1,15 +1,6 @@
 #pragma once
 
-#include <hardware/exception.h>
-#include <hardware/structs/systick.h>
-
-#include <atomic>
-#include <cstddef>
-#include <cstdlib>
-#include <functional>
 #include <memory>
-#include <mutex>
-#include <stdexcept>
 
 #include "ITimeSource.h"
 #include "mutex.h"
@@ -17,47 +8,25 @@
 namespace adsbee::time {
 
 class SysTickTimeSource : ITimeSource {
-    SysTickTimeSource(bool external)
-        : _external(external),
-          CLOCK_RESET_VAL(_external ? 12000000 /* 1 seconds at 12MHz */ : 12500000 /* 0.1 seconds at 125Mhz */) {
-        systick_hw->csr = 0 |                      /*enable, false*/
-                          (1 << 1) |               /*interrupt enable, true*/
-                          (external ? 0 : 1 << 2); /*clock source*/
-
-        systick_hw->rvr = CLOCK_RESET_VAL;
-        systick_hw->cvr = 0; /* Clear current value */
-        systick_hw->csr = systick_hw->csr | 1;
-
-        exception_set_exclusive_handler(SYSTICK_EXCEPTION, handleIrq);
-    }
-
-    SysTickTimeSource(const SysTickTimeSource&) = delete;
-    SysTickTimeSource& operator=(const SysTickTimeSource&) = delete;
-    SysTickTimeSource(SysTickTimeSource&&) = delete;
-    SysTickTimeSource& operator=(SysTickTimeSource&&) = delete;
-    friend class SysTimeTimeSourceFactory;
-
    public:
-    ~SysTickTimeSource() { systick_hw->csr = 0; }
-    uint64_t getTickCount() {
-        // todo: prevent deadlock with IRQ
-        std::lock_guard lock(_mutex);
-        return CLOCK_RESET_VAL * _interruptCount + systick_hw->cvr;
-    }
+    ~SysTickTimeSource();
+    uint64_t getTickCount();
 
    private:
-    static void handleIrq() {
-        // todo: prevent deadlock with getTickCount()
-        std::lock_guard lock(_mutex);
-        // increment on volatile vars is deprecated
-        _interruptCount = _interruptCount + 1;
-    }
+    static void handleIrq();
 
     bool _external;
     const uint32_t CLOCK_RESET_VAL;
     // We can make this static since there can only be one instance of the class
     static volatile uint64_t _interruptCount;
     static adsbee::hal::Mutex _mutex;
+
+    SysTickTimeSource(bool external);
+    SysTickTimeSource(const SysTickTimeSource&) = delete;
+    SysTickTimeSource& operator=(const SysTickTimeSource&) = delete;
+    SysTickTimeSource(SysTickTimeSource&&) = delete;
+    SysTickTimeSource& operator=(SysTickTimeSource&&) = delete;
+    friend class SysTimeTimeSourceFactory;
 };
 
 class SysTimeTimeSourceFactory {
