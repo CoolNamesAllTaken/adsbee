bc31f7dd797ad98920459a697e4659c2bc92ffea
Garrett Battaglia <gbattaglia@idinnovators.com>
2024-07-29 20:14:27 -0500

    Handling mlat time not starting at 0 systicks

--- firmware/pico/src/time/MlatTime.h
+++ firmware/pico/src/time/MlatTime.h
@@ -7,10 +7,16 @@ namespace adsbee::time {
 template <typename TimeSource, typename std::enable_if_t<std::is_base_of<ITimeSource, TimeSource>::value, bool> = true>
 class MlatTime {
    public:
-    MlatTime(std::shared_ptr<TimeSource> timeSource) : _timeSource(timeSource) {}
+    MlatTime(std::shared_ptr<TimeSource> timeSource)
+        : _timeSource(timeSource), _timeResetTicks(_timeSource->getTickCount()) {}
 
     uint64_t getCurrentTime() {
-        return convertTime(_timeSource->getFrequency(), _timeSource->getTickCount()) & TIME_BIT_MASK;
+        auto currentTicks = _timeSource->getTickCount() - _timeResetTicks;
+        return convertTime(_timeSource->getFrequency(), currentTicks) & TIME_BIT_MASK;
+    }
+
+    void onPpsPulse() {
+        // todo
     }
 
    private:
@@ -25,6 +31,7 @@ class MlatTime {
         }
     }
     std::shared_ptr<TimeSource> _timeSource;
+    uint64_t _timeResetTicks;
     static constexpr uint8_t TIME_BIT_DEPTH = 48;
     static constexpr uint64_t TIME_BIT_MASK = UINT64_MAX >> 64 - TIME_BIT_DEPTH;
 };
