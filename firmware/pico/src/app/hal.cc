d168167889ac2c8b26db73be3bfea64ad4303e3f
Garrett Battaglia <gbattaglia@idinnovators.com>
2024-08-19 20:47:48 -0500

    create clock source class to wrap systick clock

--- firmware/pico/src/app/hal.cc
+++ firmware/pico/src/app/hal.cc
@@ -1,8 +1,51 @@
 #include "hal.hh"
 
+#include <hardware/exception.h>
+#include <hardware/structs/systick.h>
+
 #include "hardware/spi.h"
 #include "pico/stdlib.h"
 
 uint64_t get_time_since_boot_us() { return to_us_since_boot(get_absolute_time()); }
 
 uint32_t get_time_since_boot_ms() { return to_ms_since_boot(get_absolute_time()); }
+
+volatile uint32_t ClockSource::_interruptCount __attribute__((aligned(4)));
+
+ClockSource::ClockSource() {
+    /* register description: ArmV6-M Manual B3.3 */
+
+    systick_hw->csr = 0 |        /* Enable: false            */
+                      (1 << 1) | /* Interrupt: true          */
+                      (1 << 2);  /* Clock source: internal   */
+
+    systick_hw->rvr = 0x00FFFFFF; /* Max counter size (24 bit)*/
+    systick_hw->cvr = 0;          /* Clear current value */
+
+    exception_set_exclusive_handler(SYSTICK_EXCEPTION, handleIrq);
+
+    systick_hw->csr = systick_hw->csr | 1; /* Start timer */
+}
+
+ClockSource::~ClockSource() { systick_hw->csr = 0; }
+
+ClockSource& ClockSource::instance() {
+    static ClockSource inst;
+    return inst;
+}
+
+uint64_t ClockSource::get48MHzTickCount() {
+    return (((_interruptCount << 24) | (0x00FFFFFF - systick_hw->cvr)) * 12) / 125;
+}
+
+uint64_t ClockSource::get12MHzTickCount() { return get48MHzTickCount() >> 2; }
+
+void ClockSource::handleIrq() {
+    // Arm doesn't provide atomic instructions in ARMv6
+    // and a Mutex would cause a deadlock with getTickCount.
+    // So we word-align the interrupt count. That _should_
+    // make the read/write a single instruction and therefore
+    // atomic. I hope...
+
+    _interruptCount = _interruptCount + 1;
+}
---
+++ firmware/pico/src/app/hal.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstdint>
+
+uint64_t get_time_since_boot_us();
+uint32_t get_time_since_boot_ms();
+
+class ClockSource {
+   public:
+    static ClockSource& instance();
+    ~ClockSource();
+    uint64_t get48MHzTickCount();
+    uint64_t get12MHzTickCount();
+
+   private:
+    static void handleIrq();
+
+    static volatile uint32_t _interruptCount;
+
+    ClockSource();
+    ClockSource(const ClockSource&) = delete;
+    ClockSource& operator=(const ClockSource&) = delete;
+    ClockSource(ClockSource&&) = delete;
+    ClockSource& operator=(const ClockSource&&) = delete;
+};
