fedea46f7bf03ab9930d97d4363ecbbde330427b
John McNelly <jkailimcnelly@gmail.com>
2024-09-24 18:23:20 -0700

    WIP heartbeat message crc not working

--- firmware/common/comms/gdl90/gdl90_utils.hh
+++ firmware/common/comms/gdl90/gdl90_utils.hh
@@ -2,79 +2,77 @@
 #define GDL90_UTILS_HH_
 
 #include "aircraft_dictionary.hh"
+#include "macros.hh"  // for MIN macro.
 
 // GDL90 is implemented based on this spec:
 // https://www.faa.gov/sites/faa.gov/files/air_traffic/technology/adsb/archival/GDL90_Public_ICD_RevA.PDF
 
-// Per the GDL90 data interface specification, the MSb of GDL90MessageType is always 0, so the value can range from
-// 0-127.
-enum GDL90MessageType : uint8_t {
+class GDL90Reporter {
+   public:
+    static const uint16_t kGDL90MessageMaxLenBytes = 200;
+    static const uint8_t kGDL90FlagByte = 0x7E;
+    static const uint8_t kGDL90ControlEscapeChar = 0x7D;
 
-};
+    static const uint16_t kGDL90CRC16Table[256];
 
-static const uint16_t kGDL90MessageMaxLenBytes = 200;
-static const uint8_t kGDL90FlagByte = 0x7E;
-static const uint8_t kGDL90ControlEscapeChar = 0x7D;
+    // Per the GDL90 data interface specification, the MSb of GDL90MessageType is always 0, so the value can range from
+    // 0-127.
+    enum GDL90MessageID : uint8_t {
+        kGDL90MessageIDHeartbeat = 0,
+        kGDL90MessageIDInitialization = 2,
+        kGDL90MessageIDUplinkData = 7,
+        kGDL90MessageIDHeightAboveTerrain = 9,
+        kGDL90MessageIDOwnshipReport = 10,
+        kGDL90MessageIDOwnshipGeometricAltitude = 11,
+        kGDL90MessageIDTrafficReport = 20,
+        kGDL90MessageIDBasicReport = 30,
+        kGDL90MessageIDLongReport = 31
+    };
 
-static const uint16_t kGDL90CRC16Table[256] = {
-    0x0,    0x1021, 0x2042, 0x3063, 0x4084, 0x50a5, 0x60c6, 0x70e7, 0x8108, 0x9129, 0xa14a, 0xb16b, 0xc18c, 0xd1ad,
-    0xe1ce, 0xf1ef, 0x1231, 0x210,  0x3273, 0x2252, 0x52b5, 0x4294, 0x72f7, 0x62d6, 0x9339, 0x8318, 0xb37b, 0xa35a,
-    0xd3bd, 0xc39c, 0xf3ff, 0xe3de, 0x2462, 0x3443, 0x420,  0x1401, 0x64e6, 0x74c7, 0x44a4, 0x5485, 0xa56a, 0xb54b,
-    0x8528, 0x9509, 0xe5ee, 0xf5cf, 0xc5ac, 0xd58d, 0x3653, 0x2672, 0x1611, 0x630,  0x76d7, 0x66f6, 0x5695, 0x46b4,
-    0xb75b, 0xa77a, 0x9719, 0x8738, 0xf7df, 0xe7fe, 0xd79d, 0xc7bc, 0x48c4, 0x58e5, 0x6886, 0x78a7, 0x840,  0x1861,
-    0x2802, 0x3823, 0xc9cc, 0xd9ed, 0xe98e, 0xf9af, 0x8948, 0x9969, 0xa90a, 0xb92b, 0x5af5, 0x4ad4, 0x7ab7, 0x6a96,
-    0x1a71, 0xa50,  0x3a33, 0x2a12, 0xdbfd, 0xcbdc, 0xfbbf, 0xeb9e, 0x9b79, 0x8b58, 0xbb3b, 0xab1a, 0x6ca6, 0x7c87,
-    0x4ce4, 0x5cc5, 0x2c22, 0x3c03, 0xc60,  0x1c41, 0xedae, 0xfd8f, 0xcdec, 0xddcd, 0xad2a, 0xbd0b, 0x8d68, 0x9d49,
-    0x7e97, 0x6eb6, 0x5ed5, 0x4ef4, 0x3e13, 0x2e32, 0x1e51, 0xe70,  0xff9f, 0xefbe, 0xdfdd, 0xcffc, 0xbf1b, 0xaf3a,
-    0x9f59, 0x8f78, 0x9188, 0x81a9, 0xb1ca, 0xa1eb, 0xd10c, 0xc12d, 0xf14e, 0xe16f, 0x1080, 0xa1,   0x30c2, 0x20e3,
-    0x5004, 0x4025, 0x7046, 0x6067, 0x83b9, 0x9398, 0xa3fb, 0xb3da, 0xc33d, 0xd31c, 0xe37f, 0xf35e, 0x2b1,  0x1290,
-    0x22f3, 0x32d2, 0x4235, 0x5214, 0x6277, 0x7256, 0xb5ea, 0xa5cb, 0x95a8, 0x8589, 0xf56e, 0xe54f, 0xd52c, 0xc50d,
-    0x34e2, 0x24c3, 0x14a0, 0x481,  0x7466, 0x6447, 0x5424, 0x4405, 0xa7db, 0xb7fa, 0x8799, 0x97b8, 0xe75f, 0xf77e,
-    0xc71d, 0xd73c, 0x26d3, 0x36f2, 0x691,  0x16b0, 0x6657, 0x7676, 0x4615, 0x5634, 0xd94c, 0xc96d, 0xf90e, 0xe92f,
-    0x99c8, 0x89e9, 0xb98a, 0xa9ab, 0x5844, 0x4865, 0x7806, 0x6827, 0x18c0, 0x8e1,  0x3882, 0x28a3, 0xcb7d, 0xdb5c,
-    0xeb3f, 0xfb1e, 0x8bf9, 0x9bd8, 0xabbb, 0xbb9a, 0x4a75, 0x5a54, 0x6a37, 0x7a16, 0xaf1,  0x1ad0, 0x2ab3, 0x3a92,
-    0xfd2e, 0xed0f, 0xdd6c, 0xcd4d, 0xbdaa, 0xad8b, 0x9de8, 0x8dc9, 0x7c26, 0x6c07, 0x5c64, 0x4c45, 0x3ca2, 0x2c83,
-    0x1ce0, 0xcc1,  0xef1f, 0xff3e, 0xcf5d, 0xdf7c, 0xaf9b, 0xbfba, 0x8fd9, 0x9ff8, 0x6e17, 0x7e36, 0x4e55, 0x5e74,
-    0x2e93, 0x3eb2, 0xed1,  0x1ef0};
+    /**
+     * Calculates the CRC for a buffer that contains a message ID and message data, then adds escapes, frames it with
+     * flag bytes, and writes it to an output buffer.
+     * @param[out] to_buf Output buffer to write to.
+     * @param[in] unescaped_message Byte buffer that includes the message ID and message data, with no escapes added and
+     * no framing bytes.
+     * @param[in] unescaped_message_len_bytes Length of unescaped_message buffer, in bytes.
+     * @retval Number of Bytes written to to_buf.
+     */
+    uint16_t WriteGDL90Message(uint8_t *to_buf, uint8_t *unescaped_message, uint8_t unescaped_message_len_bytes);
 
-/**
- * Calculate a 16-bit CRC using the pre-calculated GDL90 CRC table.
- * @param[in] buf Pointer to the message payload.
- * @param[in] buf_len_bytes Payload length in bytes.
- * @retval Calculated 16-bit CRC value.
- */
-uint16_t CalculateGDL90CRC16(uint8_t *buf, uint16_t buf_len_bytes) {
-    unsigned long int i;
-    unsigned int crc = 0;
-    for (i = 0; i < buf_len_bytes; i++) {
-        crc = kGDL90CRC16Table[crc >> 8] ^ (crc << 8) ^ buf[i];
-    }
-    return crc;
-}
+    /**
+     * Write a payload to a buffer, automatically adding escape sequences as required. Everything except for start and
+     * end of message flags should be passed in as part of the payload.
+     * @param[out] to_buf Buffer to write to.
+     * @param[in] from_buf Buffer to read from.
+     * @param[in] from_buf_len_bytes Number of bytes to read in.
+     * @retval Number of bytes written to to_buf.
+     */
+    uint16_t WriteBufferWithGDL90Escapes(uint8_t *to_buf, const uint8_t *from_buf, uint16_t from_buf_num_bytes);
 
-/**
- * Write a payload to a buffer, automatically adding escape sequences as required. Everything except for start and end
- * of message flags should be passed in as part of the payload.
- * @param[out] to_buf Buffer to write to.
- * @param[in] from_buf Buffer to read from.
- * @param[in] from_buf_len_bytes Number of bytes to read in.
- * @retval Number of bytes written to to_buf.
- */
-uint16_t WriteBufferWithGDL90Escapes(uint8_t to_buf[], const uint8_t from_buf[], uint16_t from_buf_num_bytes) {
-    uint16_t to_buf_num_bytes = 0;
-    for (uint16_t i = 0; i < from_buf_num_bytes; i++) {
-        if (from_buf[i] == kGDL90FlagByte || from_buf[i] == kGDL90ControlEscapeChar) {
-            // Escape any occurrence of 0x7E and 0x7D.
-            to_buf[to_buf_num_bytes++] = kGDL90ControlEscapeChar;
-            to_buf[to_buf_num_bytes++] = from_buf[i] ^ 0x20;
-        } else {
-            // Write other values as normal.
-            to_buf[to_buf_num_bytes++] = from_buf[i];
+    uint16_t WriteGDL90HeartbeatMessage(uint8_t *to_buf, uint32_t timestamp_sec_since_0000z, uint16_t message_counts);
+    uint16_t WriteGDL90OwnshipReport(uint8_t *to_buf, float lat_deg, float lon_deg, float alt_ft);
+
+    // uint16_t AircraftToGDL90Frame(const Aircraft &aircraft) {}
+
+    bool gnss_position_valid = false;
+    bool maintenance_required = false;
+    bool utc_timing_is_valid = false;
+
+    /**
+     * Calculate a 16-bit CRC using the pre-calculated GDL90 CRC table.
+     * @param[in] buf Pointer to the message payload.
+     * @param[in] buf_len_bytes Payload length in bytes.
+     * @retval Calculated 16-bit CRC value.
+     */
+    uint16_t CalculateGDL90CRC16(uint8_t *buf, uint16_t buf_len_bytes) {
+        uint32_t i;
+        uint16_t crc = 0;
+        for (i = 0; i < buf_len_bytes; i++) {
+            crc = kGDL90CRC16Table[crc >> 8] ^ (crc << 8) ^ buf[i];
         }
+        return crc;
     }
-    return to_buf_num_bytes;
-}
-
-// uint16_t AircraftToGDL90Frame(const Aircraft &aircraft) {}
+};
 
 #endif
