3b820e1c5f1214478b6548efdab0a312c9d8715b
John McNelly <jkailimcnelly@gmail.com>
2025-06-29 22:51:51 -0700

    Limit OTA write chunk attempts and improve on-device unit tests
    * Add additional repetitions of MLAT timing tests.
    * Improve CC1312 guards in tests.

--- firmware/pico/application/target_test/test_clocks.cc
+++ firmware/pico/application/target_test/test_clocks.cc
@@ -2,10 +2,13 @@
 #include "hardware_unit_tests.hh"
 
 UTEST(Clocks, Test48MHzMLATCounter) {
-    uint64_t mlat_counter_start = adsbee.GetMLAT48MHzCounts();
-    sleep_ms(1);
-    // Expect MLAT counter to increment by at least 48000.
-    uint64_t mlat_counter_end = adsbee.GetMLAT48MHzCounts();
-    uint64_t mlat_counter_delta = mlat_counter_end - mlat_counter_start;
-    EXPECT_NEAR(48'000, mlat_counter_delta, 4'000);
+    static const uint32_t kTestNumRepeats = 10;
+    for (uint32_t i = 0; i < kTestNumRepeats; i++) {
+        uint64_t mlat_counter_start = adsbee.GetMLAT48MHzCounts();
+        sleep_ms(1);
+        // Expect MLAT counter to increment by at least 48000.
+        uint64_t mlat_counter_end = adsbee.GetMLAT48MHzCounts();
+        uint64_t mlat_counter_delta = mlat_counter_end - mlat_counter_start;
+        EXPECT_NEAR(48'000, mlat_counter_delta, 100);
+    }
 }
