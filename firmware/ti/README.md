31e16c9bf19e5f6c7e9c07c1be5c828602e53b07
John McNelly <jkailimcnelly@gmail.com>
2025-08-30 15:25:27 -0700

    Rx working, invalid packets, subg only works after enable is toggled

---
+++ firmware/ti/README.md
@@ -0,0 +1,7 @@
+# CC1312 Firmware
+
+## Documentation
+
+[CC13x2 Proprietary RF User's Guide](https://software-dl.ti.com/simplelink/esd/simplelink_cc13x2_sdk/2.30.00.45/exports/docs/proprietary-rf/proprietary-rf-users-guide/proprietary-rf-guide/index-cc13x2.html)
+
+[CC1312 Technical Reference Manual](https://www.ti.com/lit/ug/swcu185g/swcu185g.pdf)
--- firmware/ti/sub_ghz_radio/sub_ghz_radio/uat_packet_decoder.cpp
+++ firmware/ti/sub_ghz_radio/sub_ghz_radio/uat_packet_decoder.cpp
@@ -14,10 +14,18 @@ bool UATPacketDecoder::Update()
         raw_uat_adsb_packet_queue.Dequeue(packet);
         // Decode the packet and enqueue the result.
         DecodedUATADSBPacket decoded_packet = DecodedUATADSBPacket(packet);
+
+        char raw_packet_buffer[2 * RawUATADSBPacket::kLongADSBMessageNumBytes + 1];
+        for (uint16_t i = 0; i < RawUATADSBPacket::kLongADSBMessageNumBytes; i++)
+        {
+            sprintf(raw_packet_buffer + (2 * i), "%02X", packet.encoded_message[i]);
+        }
+        raw_packet_buffer[2 * RawUATADSBPacket::kLongADSBMessageNumBytes] = '\0';
+
         if (decoded_packet.IsValid())
         {
             pico_ll.BlinkSubGLED();
-            CONSOLE_INFO("UATPacketDecoder::Update", "[%dFIXD     ] mdb_tc=%d icao=0x%06x", decoded_packet.GetRawPtr()->sigq_bits, decoded_packet.header.mdb_type_code, decoded_packet.header.icao_address);
+            CONSOLE_INFO("UATPacketDecoder::Update", "[%dFIXD     ] mdb_tc=%d icao=0x%06x buf=%s", decoded_packet.GetRawPtr()->sigq_bits, decoded_packet.header.mdb_type_code, decoded_packet.header.icao_address, raw_packet_buffer);
             if (!object_dictionary.decoded_uat_adsb_packet_queue.Enqueue(decoded_packet))
             {
                 CONSOLE_ERROR("UATPacketDecoder::Update", "Failed to enqueue decoded UAT ADS-B packet.");
@@ -25,7 +33,7 @@ bool UATPacketDecoder::Update()
         }
         else
         {
-            CONSOLE_INFO("UATPacketDecoder::Update", "[     INVLD]");
+            CONSOLE_INFO("UATPacketDecoder::Update", "[     INVLD] buf=%s", raw_packet_buffer);
         }
     }
     return true;
