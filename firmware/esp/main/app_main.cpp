76a7b80a68110d6aca2ab636c8bbcb18ee4c7c5f
John McNelly <jkailimcnelly@gmail.com>
2025-09-22 02:25:48 -0700

    Add stacktrace for memory leak detection on ESP32

--- firmware/esp/main/app_main.cpp
+++ firmware/esp/main/app_main.cpp
@@ -16,6 +16,7 @@
 #include "comms.hh"
 #include "driver/gpio.h"
 #include "driver/spi_slave.h"
+#include "esp_debug_helpers.h"  // For esp_backtrace_print
 #include "esp_log.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
@@ -35,8 +36,17 @@ ADSBeeServer adsbee_server = ADSBeeServer();
 SettingsManager settings_manager = SettingsManager();
 CommsManager comms_manager = CommsManager({});
 
+void heap_caps_alloc_failed_hook(size_t requested_size, uint32_t caps, const char *function_name) {
+    printf("%s was called but failed to allocate %d bytes with 0x%lX capabilities.\n", function_name, requested_size,
+           caps);
+    printf("Stack trace at allocation failure:\n");
+    esp_backtrace_print(10);  // Print up to 10 stack frames
+}
+
 // Main application
 extern "C" void app_main(void) {
+    esp_err_t error = heap_caps_register_failed_alloc_callback(heap_caps_alloc_failed_hook);
+
     ESP_LOGI("app_main", "Beginning ADSBee Server Application.");
     ESP_LOGI("app_main", "Default task priority: %d", uxTaskPriorityGet(NULL));
 
