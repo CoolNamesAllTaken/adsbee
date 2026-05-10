7a2565778b926703afde9c20cd414b663a2d8ff8
John McNelly <jkailimcnelly@gmail.com>
2026-05-10 04:49:03 -0700

    Make aircraft dictionary max num aircraft configurable with compile constant

--- firmware/common/adsb/aircraft_dictionary_config.hh
+++ firmware/common/adsb/aircraft_dictionary_config.hh
@@ -3,4 +3,8 @@
 
 // Maximum number of aircraft simultaneously tracked by AircraftDictionary.
 // Referenced by both aircraft_dictionary.hh and the comms reporting snapshot infrastructure.
-static constexpr uint16_t kAircraftDictionaryMaxNumAircraft = 200;
+// Override at compile time by defining AIRCRAFT_DICTIONARY_MAX_NUM_AIRCRAFT.
+#ifndef AIRCRAFT_DICTIONARY_MAX_NUM_AIRCRAFT
+#define AIRCRAFT_DICTIONARY_MAX_NUM_AIRCRAFT 200
+#endif
+static constexpr uint16_t kAircraftDictionaryMaxNumAircraft = AIRCRAFT_DICTIONARY_MAX_NUM_AIRCRAFT;
